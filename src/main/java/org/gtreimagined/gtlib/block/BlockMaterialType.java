14b2c06c7e856589985e907e9e235e00dfa3350e
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-09-11 23:09:06 -0400

    started converting blocks to use new model system

--- src/main/java/org/gtreimagined/gtlib/block/BlockMaterialType.java
+++ src/main/java/org/gtreimagined/gtlib/block/BlockMaterialType.java
@@ -1,6 +1,13 @@
 package org.gtreimagined.gtlib.block;
 
+import com.google.gson.JsonObject;
 import lombok.Getter;
+import net.minecraft.resources.ResourceLocation;
+import org.gtreimagined.gtlib.GTLib;
+import org.gtreimagined.gtlib.Ref;
+import org.gtreimagined.gtlib.client.GTLibModelManager;
+import org.gtreimagined.gtlib.datagen.builder.GTBlockModelBuilder;
+import org.gtreimagined.gtlib.datagen.providers.GTBlockStateProvider;
 import org.gtreimagined.gtlib.material.IMaterialObject;
 import org.gtreimagined.gtlib.material.Material;
 import org.gtreimagined.gtlib.material.MaterialColorChanger;
@@ -44,7 +51,15 @@ public class BlockMaterialType extends BlockBasic implements IColorHandler, IMat
     }
 
     @Override
-    public Texture[] getTextures() {
-        return !textureFolder.isEmpty() ? new Texture[]{new Texture(domain, "block/" + textureFolder + "/" + material.getId())} : material.getSet().getTextures(type);
+    public void onBlockModelBuild(Block block, GTBlockStateProvider prov) {
+        GTBlockModelBuilder b = prov.getBuilder(block);
+        if (!textureFolder.isEmpty()){
+            b.parent(new ResourceLocation(Ref.ID, "block/preset/simple"));
+            b.texture("all", new Texture(domain, "block/" + textureFolder + "/" + material.getId()));
+        } else {
+            b.loader(GTLibModelManager.LOADER_FALLBACK);
+            b.property("base", getMaterial().getSet().getDomain() + ":block/material/" + getMaterial().getSet().getId() + "/" + type.getId())
+                    .property("fallback", Ref.ID + ":block/material/none/" + type.getId());
+        }
     }
 }
